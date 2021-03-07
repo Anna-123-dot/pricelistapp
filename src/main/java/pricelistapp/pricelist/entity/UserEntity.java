d9b57243a7ff361f72a6f33229eb9455aa16f7c3
Anna <aprusik@tlen.pl>
2021-03-07 11:13:33 +0100

    changes in index.html, contact.html, in application.properties connection with MySQL database added, new classes AdminController, SecurityConfig, RoleEntity, UserEntity were created, also styles added (bootstrap.min.css), products.html added

---
+++ src/main/java/pricelistapp/pricelist/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package pricelistapp.pricelist.entity;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity(name = "Users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"
+            )
+    )
+    private Collection<RoleEntity> roles;
+}
